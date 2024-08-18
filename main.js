b380d8ae77b952b8c8648abbcf0a387aad714db4
nakamura <hrm.nkmr04@gmail.com>
2024-08-18 20:00:19 +0900

    third commit

--- main.js
+++ main.js
@@ -1,47 +1,17 @@
-var obj = {
-	showText: function() {
-		console.log('Hello World');
-	}
-}
-obj.showText();
-var obj = {
-	showText: function(name) {
-		console.log('こんにちは、' + name + 'さん！');
-	}
-}
-obj.showText('太郎');
-var obj = {
-	showText: function(num) {
-		var result = num * num;
-		//計算した結果を戻り値として返す
-		return result;
-	}
-}
-console.log(obj.showText(10));
-
-function peach(price, func) {
-	const name = "ピーチ";
-	func(name, price);
-}
-const price = function price(name, price) {
-	console.log(name + " の値段は" + price + "円です。");
-}
-peach(500, price);
-
-function addTax(price, func) {
-	const taxPrice = Math.round(price * 1.10);
-	func(taxPrice);
-	return addTax
-}
-
-function displayPrice(name, price) {
-	console.log(name + " の値段は" + price + "円です。");
+function addTax(price) {
+	return Math.round(price * 1.10); // 税込み価格を計算して返す
 }
 // トマトの税込み価格をコンソールに表示させる処理
-addTax(169, function(price) {
-	displayPrice("トマト", price);
-});
+{
+	const name = "トマト";
+	const price = 200;
+	const taxPrice = addTax(price);
+	console.log(name + " の値段は " + taxPrice + "円です。"); // "トマト の値段は 200円です。" 
+}
 // 玉ねぎの税込み価格をコンソールに表示させる処理
-addTax(170, function(price) {
-	displayPrice("玉ねぎ", price);
-});
+{
+	const name = "玉ねぎ";
+	const price = 300;
+	const taxPrice = addTax(price);
+	console.log(name + " の値段は " + taxPrice + "円です。"); // "玉ねぎ の値段は 300円です。" 
+}
