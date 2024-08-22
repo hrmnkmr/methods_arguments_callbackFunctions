caf87fb6a2372df0e1751220c73e4993c83ecb42
nakamura <hrm.nkmr04@gmail.com>
2024-08-23 07:21:59 +0900

    fifth commit

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-// 税込価格を計算する関数
-function addTax(price) {
-	return Math.round(price * 1.10); // 税率10%を適用し、四捨五入して税込価格を計算
+function addTax(price, name, callback) {
+	const taxPrice = Math.round(price * 1.10); // 税率10%を適用し、四捨五入して税込価格を計算
+	callback(name, taxPrice); // 計算結果をコールバック関数に渡す
 }
 // 税込価格を表示する関数
-function displayPrice(name, price) {
-	const taxPrice = addTax(price); // 税込価格を計算
+function displayPrice(name, taxPrice) {
 	console.log(name + " の税込価格は " + taxPrice + "円です。");
 }
 // トマトの税込み価格をコンソールに表示させる処理
-displayPrice("トマト", 200);
+addTax(200, "トマト", displayPrice);
 // 玉ねぎの税込み価格をコンソールに表示させる処理
-displayPrice("玉ねぎ", 300);
+addTax(300, "玉ねぎ", displayPrice);
